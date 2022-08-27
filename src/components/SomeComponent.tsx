e5de0be85b896500d53b958aa58eb2b498623da4
laj <larry.jones@reveal-energy.com>
2022-08-27 14:10:22 -0500

    Finally, all working!

--- src/components/SomeComponent.tsx
+++ src/components/SomeComponent.tsx
@@ -1,24 +1,21 @@
 import {useContext} from 'react';
-import {useActor} from '@xstate/react';
+import {StateFrom} from "xstate";
+import {useSelector} from '@xstate/react';
 import {GlobalStateContext} from "./GlobalState";
+import {acnMachine} from "../machines/AcnMachine";
+
+const loggedInSelector = (state: StateFrom<typeof acnMachine>) => {
+    return state.matches('loggedIn');
+}
 
 
 export const SomeComponent = (_props: {}) => {
     const globalServices = useContext(GlobalStateContext);
-    const [state] = useActor(globalServices.acnService);
-
-    console.log('In SomeComponent()');
-
-    const renderState = (theState: typeof state) => {
-        console.log(`InSomeComponent: state.matches("loggedIn")=${state.matches("loggedIn")}`);
-        const result = theState.matches('loggedIn)') ? 'Logged In' : 'Logged Out';
-        console.log(`InSomeComponent.renderState returns ${result}`);
-        return result;
-    }
+    const isLoggedIn = useSelector(globalServices.acnService, loggedInSelector);
 
     return (
         <>
-            <p>{ renderState(state) }</p>
+            <p>{isLoggedIn ? 'Logged In' : 'Logged Out'}</p>
             <button onClick={() => {
                 const nextState = globalServices.acnService.send('LOG_IN')
                 console.log(nextState);
