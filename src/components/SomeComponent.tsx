3849c6f28fc56c7fb9843e5ccb9eca6e9a53d3f8
laj <larry.jones@reveal-energy.com>
2022-08-27 14:49:29 -0500

    Reduce code duplication through HOF

--- src/components/SomeComponent.tsx
+++ src/components/SomeComponent.tsx
@@ -1,11 +1,16 @@
 import {useContext} from 'react';
-import {StateFrom} from "xstate";
+import {EventFrom, InterpreterFrom, StateFrom} from "xstate";
 import {useSelector} from '@xstate/react';
 import {GlobalStateContext} from "./GlobalState";
 import {acnMachine} from "../machines/AcnMachine";
 
 const loggedInSelector = (state: StateFrom<typeof acnMachine>) => {
     return state.matches('loggedIn');
+}
+
+const makeSender = (interpreter: InterpreterFrom<typeof acnMachine>, eventName: string) => {
+    const sender = () => interpreter.send({ type: eventName } as EventFrom<typeof acnMachine>);
+    return sender;
 }
 
 
@@ -16,10 +21,10 @@ export const SomeComponent = (_props: {}) => {
     return (
         <>
             <p>{isLoggedIn ? 'Logged In' : 'Logged Out'}</p>
-            <button onClick={() => globalServices.acnService.send('LOG_IN')}>
+            <button onClick={makeSender(globalServices.acnService, 'LOG_IN' )}>
                 Log In
             </button>
-            <button onClick={() => globalServices.acnService.send('LOG_OUT')}>
+            <button onClick={makeSender(globalServices.acnService, 'LOG_OUT' )}>
                 Log Out
             </button>
         </>
